a48d149ed6b0c1116816cea3dfc9337a99e8a347
Volodimir <vova.stepanenko2020@gmail.com>
2022-01-20 18:43:41 +0200

    del unuse Mapper

--- src/main/java/com/example/exchange/controller/CurrencyController.java
+++ src/main/java/com/example/exchange/controller/CurrencyController.java
@@ -12,18 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyController {
 
     CurrencyService currencyService;
-    CurrencyMapper currencyMapper;
 
     public CurrencyController(CurrencyService currencyService, CurrencyMapper currencyMapper) {
         this.currencyService = currencyService;
-        this.currencyMapper = currencyMapper;
     }
 
     @GetMapping("/currency-codes")
     public AvailableCode currencyCodes(){
-
         AvailableCode availableCode = new AvailableCode();
-
         currencyService.getAll().forEach(currency -> availableCode.availableCode.add(currency.getCode()));
         return availableCode;
     }
