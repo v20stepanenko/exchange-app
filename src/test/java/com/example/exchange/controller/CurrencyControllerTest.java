cac3faace1ed1273af1d1bb58da1805004fafab0
Volodimir <vova.stepanenko2020@gmail.com>
2022-01-24 14:55:16 +0200

    minor fix

--- src/test/java/com/example/exchange/controller/CurrencyControllerTest.java
+++ src/test/java/com/example/exchange/controller/CurrencyControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.exchange.controller;
 
-import com.example.exchange.ExchangeAppApplication;
 import com.example.exchange.dao.CurrencyDAO;
 import com.example.exchange.dao.HistoryDAO;
 import com.example.exchange.model.Currency;
@@ -13,13 +12,10 @@ import javax.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
